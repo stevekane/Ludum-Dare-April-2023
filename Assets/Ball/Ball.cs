b9d97f7cbec9a0854ccf8d198561bb5c1c0f3977
Steve Kane <kanesteven@gmail.com>
2023-04-29 15:07:33 -0500

    chopped up toss into subclips and added them to player animator controller

--- Assets/Ball/Ball.cs
+++ Assets/Ball/Ball.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using Cinemachine;
 
 public class Ball : MonoBehaviour {
   public TrailRenderer TrailRenderer;
   public Rigidbody Rigidbody;
   public int HitStopFrames;
   public Vector3 StoredVelocity;
+
+  void Start() {
+    FindObjectOfType<CinemachineTargetGroup>().AddMember(transform, 1, 1);
+  }
+
+  void OnDestroy() {
+    FindObjectOfType<CinemachineTargetGroup>().RemoveMember(transform);
+  }
 
   void FixedUpdate() {
     if (HitStopFrames == 0) {
