78ed55c6f77f434ed870fdd531216d056282abda
Matt Perry <mpcomplete@gmail.com>
2023-04-30 17:04:40 -0400

    Encounter uses text asset

--- Assets/Mobs/EncounterManager.cs
+++ Assets/Mobs/EncounterManager.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 public class Parser {
   public string Text;
   int Pos = 0;
+  int Line = 0;
 
   public Parser(string text) => Text = text;
 
+  public string Location => $"Line {Line}, position {Pos}";
   public bool EOF => Pos == Text.Length;
   public char ReadChar() {
-    return Text[Pos++];
+    if (Text[Pos++] is var c && c == '\n')
+      Line++;
+    return c;
   }
   public int ReadInt() {
     return int.Parse(ReadLine());
   }
   public string ReadLine() {
     var start = Pos;
-    while (ReadChar() is var c && c != '\n')
+    while (!EOF && ReadChar() is var c && c != '\n')
       ;
-    return Text.Substring(start, Pos - start - 1);
-  }
-  public void SkipSpace() {
-    while (!EOF && char.IsWhiteSpace(Text[Pos]))
-      Pos++;
+    return Text.Substring(start, Mathf.Max(0, Pos - start - 1));
   }
 }
 
@@ -37,26 +37,30 @@ class Wave {
   int Delay;
   List<Spawn> Spawns = new();
 
-  public static Wave Parse(Parser parser, Dictionary<char, GameObject> objectMap) {
+  public static Wave Parse(Parser parser, Transform center, Dictionary<char, GameObject> objectMap) {
     if (parser.EOF)
       return null;
     var wave = new Wave();
-    wave.Delay = parser.ReadInt();
-    for (int i = 0; i < 4; i++) {
-      var line = parser.ReadLine();
-      for (int j = 0; j < line.Length; j++) {
-        if (objectMap.TryGetValue(line[j], out var obj)) {
-          wave.Spawns.Add(new Spawn { Prefab = obj, Position = GetPosition(i, j) });
-        } else if (!char.IsWhiteSpace(line[j])) {
-          Debug.LogWarning($"Unknown object code in encounter: {line[j]}");
+    try {
+      wave.Delay = parser.ReadInt();
+      for (int i = 0; i < 4; i++) {
+        var line = parser.ReadLine();
+        for (int j = 0; j < line.Length; j++) {
+          if (objectMap.TryGetValue(line[j], out var obj)) {
+            wave.Spawns.Add(new Spawn { Prefab = obj, Position = GetPosition(center, i, j) });
+          } else if (!char.IsWhiteSpace(line[j])) {
+            Debug.LogWarning($"Unknown object code in encounter: {line[j]}");
+          }
         }
       }
+    } catch (Exception e) {
+      Debug.LogError($"Parse error at {parser.Location}: {e}");
     }
     return wave;
   }
 
-  static Vector3 GetPosition(int y, int x) {
-    return new(x*5 - 7.5f, 20f - y*5, 80f);
+  static Vector3 GetPosition(Transform center, int y, int x) {
+    return center.position + new Vector3(x*5 - 7.5f, 20f - y*5, 0f);
   }
 
   public async Task Run(TaskScope scope) {
@@ -70,9 +74,9 @@ class Wave {
 public class Encounter {
   List<Wave> Waves = new();
 
-  public static Encounter Parse(Parser parser, Dictionary<char, GameObject> objectMap) {
+  public static Encounter Parse(Parser parser, Transform center, Dictionary<char, GameObject> objectMap) {
     var encounter = new Encounter();
-    while (Wave.Parse(parser, objectMap) is var wave && wave != null) {
+    while (Wave.Parse(parser, center, objectMap) is var wave && wave != null) {
       encounter.Waves.Add(wave);
     }
     return encounter;
@@ -91,33 +95,22 @@ public class EncounterManager : MonoBehaviour {
     public GameObject Prefab;
   }
 
+  [SerializeField] TextAsset File;
   [SerializeField] ObjectMap[] ObjectMappings;
   Dictionary<char, GameObject> ObjectMappingsDict = new();
   Encounter Encounter = new();
   TaskScope Scope = new();
 
-  [SerializeField, TextArea(10, 50)] string Test = @"5
-R  G
- B  
-
-R  G
-10
- G
- G
-
-BBBB
-";
-
-  public Encounter Parse(string descr) {
+  public Encounter Parse(string descr, Transform center) {
     var parser = new Parser(descr);
-    return Encounter.Parse(parser, ObjectMappingsDict);
+    return Encounter.Parse(parser, center, ObjectMappingsDict);
   }
 
   void Start() {
     ObjectMappingsDict = new();
     foreach (var m in ObjectMappings)
       ObjectMappingsDict[m.Code] = m.Prefab;
-    Encounter = Parse(Test);
+    Encounter = Parse(File.text, transform);
     Scope.Run(Encounter.Run);
   }
 
