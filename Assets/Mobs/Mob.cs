490ed39b2baf8f3d3a6c39ce63f43b5a3204fe05
Matt Perry <mpcomplete@gmail.com>
2023-05-01 14:32:36 -0400

    HurtBuffer

--- Assets/Mobs/Mob.cs
+++ Assets/Mobs/Mob.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Mob : MonoBehaviour {
@@ -11,6 +12,9 @@ public class Mob : MonoBehaviour {
 
   public EventSource OnDeath { get; private set; } = new();
 
+  int HurtBufferTicks = Timeval.FromMillis(200).Ticks;
+  List<(HurtType, int)> HurtBuffer = new();
+
   public int SequenceIdx = 0;
   public int RegenTicks = 0;
   public int RegeneratingRing => Mathf.Max(SequenceIdx-1, 0);
@@ -18,8 +22,10 @@ public class Mob : MonoBehaviour {
   static int MaxTotalTicks = Timeval.FromSeconds(3f).Ticks;
 
   public void OnHurt(HurtType type) {
-    if (type != HurtSequence[SequenceIdx])
+    if (type != HurtSequence[SequenceIdx]) {
+      HurtBuffer.Add((type, Timeval.TickCount));
       return;
+    }
     RegenTicks = RegenDuration.Ticks;
     if (++SequenceIdx == HurtSequence.Length)
       Die();
@@ -31,6 +37,13 @@ public class Mob : MonoBehaviour {
   }
 
   void FixedUpdate() {
+    // Remove old hurtbuffer entries.
+    HurtBuffer.RemoveAll(e => e.Item2 + HurtBufferTicks < Timeval.TickCount);
+    while (SequenceIdx < HurtSequence.Length && HurtBuffer.FindIndex(e => e.Item1 == HurtSequence[SequenceIdx]) is var idx && idx >= 0) {
+      HurtBuffer.RemoveAt(idx);
+      OnHurt(HurtSequence[SequenceIdx]);
+    }
+
     if (--RegenTicks <= 0) {
       if (RegeneratingRing > 0) {
         SequenceIdx--;
@@ -79,5 +92,13 @@ public class Mob : MonoBehaviour {
     } else {
       return outerRadius;
     }
+  }
+
+  TaskScope Scope = new();
+  [ContextMenu("Redblue")]
+  async void Test() {
+    OnHurt(HurtType.Red);
+    await Scope.Ticks(5);
+    OnHurt(HurtType.Blue);
   }
 }
